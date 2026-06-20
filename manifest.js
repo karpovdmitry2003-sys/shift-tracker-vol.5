7e739a8c9eea5f40a119018bc068450138e701df
karpovdmitry2003-sys <karpovdmitry2003@gmail.com>
2026-06-20 18:41:07 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+  "name": "Shift Tracker v6",
+  "short_name": "Shifts",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0f0f0f",
+  "theme_color": "#2b7cff"
+}
